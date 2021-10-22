fe8c5448b719b369cf6ae69f75b1937d46b6c525
sreedevi <sreedevi0902@gmail.com>
2021-10-22 22:25:48 +0530

    added initial commit

---
+++ src/main/java/com/democompany/demo_project_1/App.java
@@ -0,0 +1,13 @@
+package com.democompany.demo_project_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
